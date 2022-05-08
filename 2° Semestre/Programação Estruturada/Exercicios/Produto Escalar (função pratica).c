9cbc2ddadfa171c3a0fb61fd7a2f2e0af689928b
Julio Cesar Bonow Manoel <84098213+JulioManoel@users.noreply.github.com>
2022-05-08 16:45:28 -0300

    Atualização

---
+++ 2° Semestre/Programação Estruturada/Exercicios/Produto Escalar (função pratica).c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void mostraVetor(int vet[2][5]);
+int calcularVetor(int vet[2][5]);
+
+void main(){
+	int a[5],b[5],i;
+	printf("\nVetor A");
+	for(i=0;i<5;i++){
+		printf("\nDigite o valor [%d]: ", i+1);
+		scanf("%i", &a[i]);
+		}
+	}
+	for(i=0;i<5;i++){
+		printf("\nDigite o valor [%d]: ", i+1);
+		scanf("%i", &b[i]);
+		}
+	}
+	printf("\nVetor A");
+	
+	mostraVetor(produto);
+}
+
+void mostraVetor(int vet[2][5]){
+	int i,j;
+	for(i=0;i<2;i++){
+		for(j=0;j<5;j++){
+			printf("%i\t", vet[i]);
+		}
+		printf("\n");
+	}
+}
+
+int calcularVetor(int vet[2][5]){
+	int i,j;
+}
