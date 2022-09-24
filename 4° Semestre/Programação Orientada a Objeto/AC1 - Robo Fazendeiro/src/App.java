9bba2f6b887b1e9b5a7dbb705dda1546baa9a272
Julio Cesar Bonow Manoel <84098213+JulioManoel@users.noreply.github.com>
2022-09-24 12:22:56 -0300

    Atualização

--- 4° Semestre/Programação Orientada a Objeto/AC1 - Robo Fazendeiro/src/App.java
+++ 4° Semestre/Programação Orientada a Objeto/AC1 - Robo Fazendeiro/src/App.java
@@ -1,42 +1,5 @@
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) throws Exception {
         
-        int numEstacao, numComandos, devastada, pos = 1, permaneceu = 0;
-        Scanner input = new Scanner(System.in);
-        
-        do{
-            System.out.print("Digite o número de Estações (2-100): ");
-            numEstacao = input.nextInt();
-        }while(numEstacao < 1 && numEstacao > 101);
-        
-        do{
-            System.out.print("Digite o número de Comandos (1-1000): ");
-            numComandos = input.nextInt();
-        }while(numComandos < 0 && numComandos > 1001);
-        
-        System.out.print("Digite o número da Estação de Vastada: ");
-        devastada = input.nextInt();
-
-        for(int i=0;i<numComandos;i++){
-            if(pos == devastada){
-                permaneceu++;
-            }
-            pos += input.nextInt();
-            if(pos > numEstacao){
-                pos = 1;
-            }
-            else if(pos == 0){
-                pos = numEstacao;
-            }
-        }
-        
-        if(pos == devastada){
-            permaneceu++;
-        }
-
-        System.out.println("O robô permaneceu " + permaneceu + " vezes");
-        input.close();
     }
 }
